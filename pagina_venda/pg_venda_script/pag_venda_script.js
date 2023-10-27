5b954e5a1381acf163bd9a0bfa090cd5a764441f
Wesley Medeiros <da.rosa.wesley25@gmail.com>
2023-10-27 00:07:33 -0300

    Responsivo finalizado

--- pagina_venda/pg_venda_script/pag_venda_script.js
+++ pagina_venda/pg_venda_script/pag_venda_script.js
@@ -1,13 +1,14 @@
  $(function(){
     $('#icone_menu').on('click', ()=>{
-        $('.menu').slideDown(1500)
-        $('nav #icone_menu').remove()
+        $('.menu').slideDown(2000)
+        $('nav .menu').insertAfter('#icone_menu')
+        $('nav #icone_menu').css('display', 'none')
         $('.icone_close').css('display', 'block')
     })
     $('.icone_close').on('click', ()=>{
-        $('.menu').slideUp(1500)
-        $('nav .icone_close').remove()
-        $('#icone_menu').css('display', 'block')
+        $('.menu').slideUp(2000)
+        $('nav .menu').insertBefore('#icone_menu')
+        $('nav #icone_menu').css('display', 'block')
+        $('.icone_close').css('display', 'none')
     })
-
 })
